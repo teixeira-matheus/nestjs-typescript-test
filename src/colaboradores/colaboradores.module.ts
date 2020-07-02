5b9d1eff67a7b92fd80ea821a460d9a9ceacd6cd
Matheus Matos Teixeira <teixeirammatheus@gmail.com>
2020-07-01 23:54:43 -0300

    Importing entities into TypeORM

--- src/colaboradores/colaboradores.module.ts
+++ src/colaboradores/colaboradores.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ColaboradoresService } from './colaboradores/colaboradores.service';
 import { ColaboradoresController } from './colaboradores/colaboradores.controller';
+import { Colaborador } from './colaborador.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Colaborador]),
+  ],
   providers: [ColaboradoresService],
   controllers: [ColaboradoresController]
 })
